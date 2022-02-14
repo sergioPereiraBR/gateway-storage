fe411575e0147902492efcd6882e3cc36868e843
sergioPereiraBR <sergiopereira.br@hotmail.com>
2022-02-14 03:11:41 -0300

    gateway start

---
+++ ssp/src/test/java/com/gateway/storage/ssp/SspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.storage.ssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
